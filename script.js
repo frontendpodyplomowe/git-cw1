b3a7587a595ed51084b4ebf9fd9d75abb6340990
Dobry1995 <kamdob1995@gmail.com>
2022-12-19 23:10:30 +0100

    background image added, textRegulation added, simply js script added, few css updates to regulations dialog box added

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-const showButton = document.querySelector('#showDialog');
-const favDialog = document.querySelector('#favDialog');
-const outputBox = document.querySelector('#output');
+const showButton = document.querySelector("#showDialogBtn");
+const favDialog = document.querySelector("#favDialog");
+const outputBox = document.querySelector("#output");
+const confirmBtn = document.querySelector("#confirmBtn");
+const cancelBtn = document.querySelector("#cancelBtn");
+const exitBtn = document.querySelector('#exitBtn');
+let ifConfirm = false;
 
+showButton.addEventListener("click", () => {
+  favDialog.showModal();
+});
 
+confirmBtn.addEventListener("click", () => {
+  console.log("Potwierdzono regulamin");
+  ifConfirm = true;
+  checkAnswer();
+  favDialog.close();
+});
 
+cancelBtn.addEventListener("click", () => {
+  console.log("Nie potwierdzono regulaminu");
+  ifConfirm = false;
+  checkAnswer();
+});
+
+exitBtn.addEventListener('click', () =>{
+    favDialog.close();
+})
+
+function checkAnswer() {
+  if (ifConfirm === true) {
+    confirmBtn.setAttribute("disabled", "");
+  } else if (ifConfirm === false) {
+    confirmBtn.removeAttribute("disabled", "");
+  }
+}
